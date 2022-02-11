383fec25782b1502054982889347a033b4a32ec2
Marko <mkorkea@gmail.com>
2022-02-11 19:07:44 +0200

    commit

--- 07_ws_accelerometer/public/client.js
+++ 07_ws_accelerometer/public/client.js
@@ -1,7 +1,43 @@
-
+let socket = io();
 
 const sensor_gauge = document.getElementById("sensor_gauge");
 const ctx = sensor_gauge.getContext("2d");
 
-ctx.arc(150, 150, 10, 0, Math.Pi * 2);
+
+socket.on("sensor_updated", (data)=>{
+
+const scale = 300.0 / 30.0;
+
+const screen_x = data.x * scale + 150;
+const screen_y = data.y * scale + 150;
+const screen_z = data.z * scale + 150;
+
+//console.log(screen_x);
+
+ctx.clearRect(0,0, sensor_gauge.clientWidth, sensor_gauge.height);
+
+ctx.beginPath();
+ctx.arc(screen_x, screen_y, 10, 0, Math.PI * 2);
 ctx.fill();
+
+ctx.beginPath();
+ctx.rect(300, screen_z, 50, 4);
+ctx.fill();
+
+});
+
+const on_devicemotion = (motion)=>{
+let data = {
+
+    x: motion.accelerationIncludingGravity.x,
+    y: motion.accelerationIncludingGravity.y,
+    z: motion.accelerationIncludingGravity.z,
+
+};
+
+socket.emit("sensor_data", data);
+
+};
+
+
+window.addEventListener("devicemotion", on_devicemotion, true);
