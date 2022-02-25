534b08994ff20afa317831d4622a502a8f18c902
Marko <mkorkea@gmail.com>
2022-02-25 17:20:23 +0200

    commit

--- 07_ws_accelerometer/app.js
+++ 07_ws_accelerometer/app.js
@@ -4,54 +4,47 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const PORT = process.env.PORT || 8081;
 let connections = [];
-const broadcast_sensor_data = ()=>{
-for(let id in connections){
-const connection = connections[id];
-connection.emit("sensor_uptated", data);
-}
-
+const broadcast_sensor_data = (data)=>{
+    for(let id in connections){
+        const connection = connections[id];
+        connection.emit("sensor_updated", data);
+    }
 };
-let last_data = {
 
+let current_data = {
     x:0,
     y:0,
     z:0,
-
 }
 const smooth_data = (data)=>{
-let delta_x = data.x - current_data.x;
-let delta_y = data.y - current_data.y;
-let delta_z = data.z - current_data.z;
-
-current_data.x += delta_x * 0.1;
-current_data.y += delta_y * 0.1;
-current_data.z += delta_z * 0.1;
-return current_data;
-
+    let delta_x = data.x - current_data.x;
+    let delta_y = data.y - current_data.y;
+    let delta_z = data.z - current_data.z;
+    current_data.x += delta_x * 0.1;
+    current_data.y += delta_y * 0.1;
+    current_data.z += delta_z * 0.1;
+    return current_data;
 };
 
 
+
 io.on("connection", (socket)=>{
+    connections[socket.id] = socket;
+    console.log("connected");
 
-connections[socket.id] = socket;
-console.log("connected");
+    socket.on("disconnect", (socket)=>{
+        delete connections[socket.id];
+        console.log("disconnected");
+    });
 
-socket.on("disconnect", (socket)=>{
-delete connections[socket.id];
-console.log("disconnect");
-
-});
-
-socket.on("sensor_data", (data)=>{
-console.log(data);
-
-
-});
-
+    socket.on("sensor_data", (data)=>{
+        data = smooth_data(data);
+        broadcast_sensor_data(data);
+        console.log(data);
+    });
 });
 
 app.use(express.static("public"));
 
 server.listen(PORT);
-
-console.log("Server listening port")
+console.log("Server listening port: ", PORT);
--- 07_ws_accelerometer/public/client.js
+++ 07_ws_accelerometer/public/client.js
@@ -3,41 +3,33 @@ let socket = io();
 const sensor_gauge = document.getElementById("sensor_gauge");
 const ctx = sensor_gauge.getContext("2d");
 
+socket.on("sensor_updated", (data) => {
+  const scale = 300.0 / 30.0;
 
-socket.on("sensor_updated", (data)=>{
+  const screen_x = data.x * 10 + 150;
+  const screen_y = data.y * 10 + 150;
+  const screen_z = data.z * 10 + 148;
 
-const scale = 300.0 / 30.0;
+  //console.log(screen_x);
 
-const screen_x = data.x * scale + 150;
-const screen_y = data.y * scale + 150;
-const screen_z = data.z * scale + 150;
+  ctx.clearRect(0, 0, sensor_gauge.width, sensor_gauge.height);
 
-//console.log(screen_x);
+  ctx.beginPath();
+  ctx.arc(screen_x, screen_y, 10, 0, Math.PI * 2);
+  ctx.fill();
 
-ctx.clearRect(0,0, sensor_gauge.clientWidth, sensor_gauge.height);
-
-ctx.beginPath();
-ctx.arc(screen_x, screen_y, 10, 0, Math.PI * 2);
-ctx.fill();
-
-ctx.beginPath();
-ctx.rect(300, screen_z, 50, 4);
-ctx.fill();
-
+  ctx.beginPath();
+  ctx.rect(300, screen_z, 50, 4);
+  ctx.fill();
 });
 
-const on_devicemotion = (motion)=>{
-let data = {
-
+const on_devicemotion = (motion) => {
+  let data = {
     x: motion.accelerationIncludingGravity.x,
     y: motion.accelerationIncludingGravity.y,
     z: motion.accelerationIncludingGravity.z,
-
+  };
+  socket.emit("sensor_data", data);
 };
-
-socket.emit("sensor_data", data);
-
-};
-
 
 window.addEventListener("devicemotion", on_devicemotion, true);
