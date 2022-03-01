118db596ff79c35b23d90498a231d3a4a57deba7
Marko <mkorkea@gmail.com>
2022-03-01 20:43:03 +0200

    commit

--- 08_chat/app.js
+++ 08_chat/app.js
@@ -6,36 +6,57 @@ const io = require("socket.io")(server);
 const PORT = process.env.PORT || 8081;
 
 let connections = [];
+let nicknames = [];
+
 let number_of_connections = 0;
 
 const broadcast_message = (message)=>{
-
-for(let id in connections){
-    const socket = connetions[id];
-    socket.emit("server-to-client", message);
-}
+    for(let id in connections){
+        const socket = connections[id];
+        socket.emit("new_message", message);
+    }
 };
 
+const broadcast_n_clients = ()=>{
+    for(let id in connections){
+        const socket = connections[id];
+        socket.emit("n_clients", number_of_connections);
+    }
+};
+
+const emit_nickname = (socket) =>{
+    socket.emit("nickname", nicknames[socket.id]);
+}
+
 io.on("connection", (socket)=>{
-connections[socket.id] = socket;
-number_of_connections++;
-
-//socket.emit("server-to-client", "Hello from server");
-
-console.log("client connected. number_of_connections: ", number_of_connections);
-socket.on("disconnect", (socket)=>{
-    delete connections[socket.id];
-    number_of_connections--;
+    connections[socket.id] = socket;
+    nicknames[socket.id] = socket.id;
+    emit_nickname(socket);
+    number_of_connections++;
+    broadcast_n_clients();
+    //socket.emit("server-to-client", "Hello from server");
     console.log("client connected. number_of_connections: ", number_of_connections);
-});
+    socket.on("disconnect", (socket)=>{
+        delete connections[socket.id];
+        number_of_connections--;
+        broadcast_n_clients();
+        console.log("client disconnected. number_of_connections: ", number_of_connections);
+        //
+    });
 
-socket.on("clien-to-server", (message)=>{
-    broadcast_message(message);
+    socket.on("send_message", (text)=>{
+        const message = {
+            nickname: nicknames[socket.id],
+            text: text
+        };
+        broadcast_message(message);
+    });
+
+    socket.on("set_nickname", (nickname)=>{
+        nicknames[socket.id] = nickname;
+        emit_nickname(socket);
+    });
     
-    //console.log(message);
-
-})
-
 });
 
 app.use(express.static("public"));
