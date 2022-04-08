ceebfc63513b1da2fc357c4f1ec7c9336ca33dd1
Marko <marko.korkea-aho2@seamk.fi>
2022-04-08 16:11:00 +0300

    commit

---
+++ 11_rest_api/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const alarm_controller = require("./alarm_controller")
+const body_parser = require("body-parser");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(body_parser.json());
+
+//ROUTING
+app.post("/api/alarm", alarm_controller.api_post_alarm);
+app.get("/api/alarms", alarm_controller.api_get_alarms);
+app.get("/api/alarm/:id", alarm_controller.api_get_alarm);
+app.put("/api/alarm/:id", alarm_controller.api_put_alarm);
+app.delete("/api/alarm/:id", alarm_controller.api_delete_alarm);
+
+const db_uri = "mongodb+srv://db_user:HxK0dIDDkci6Y7la@cluster0.ganue.mongodb.net/alarm_db?retryWrites=true&w=majority";
+
+mongoose.connect(db_uri, {}).then(() => {
+  console.log("db connected");
+  console.log("listening port: ", PORT);
+  app.listen(PORT);
+});
